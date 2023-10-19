7f10ed1d3a49979c28fe34994bce6f05b1c775cd
Karlan <zngr96@yadnex.ru>
2023-10-19 22:41:57 +0300

    add task2

---
+++ lesson3/task2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Внутри класса Answer напишите метод ShowCube, 
+// который принимает на вход число (N) и 
+// выводит таблицу кубов чисел от 1 до N (включительно) каждое на новой строке.
+
+void ShowCube(int num)
+{
+    int count = 1;
+    while(count <= num)
+    {
+        int sum = count * count * count;
+        Console.WriteLine(sum);
+        count++;
+    }
+}
+ShowCube(10);
