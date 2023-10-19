270a743bdc801312e8364c94af7f1f61810829f5
Karlan <zngr96@yadnex.ru>
2023-10-19 22:00:34 +0300

    add one task in lesson3

---
+++ lesson3/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿string EnterNumber(string msg)
+{
+    Console.WriteLine(msg);
+    string num = Console.ReadLine();
+    return num;
+}
+
+string IsPalindrome(string number)
+{
+    int len = number.Length;
+    int num = int.Parse(number);
+    if(len == 5)
+    {
+        // Console.WriteLine("Yes");
+        if (num / 10000 == num % 10 && num / 1000 % 10 == num % 100 / 10)
+        {
+            Console.WriteLine("True");
+        }
+        else Console.WriteLine("False");
+
+    }
+    else{ Console.WriteLine("Число не пятизначное");
+    Console.WriteLine("False");
+    }
+    return "";
+}
+
+IsPalindrome(EnterNumber("Enter num"));
