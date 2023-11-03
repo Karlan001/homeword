993b2ec1c725b75481c88104c6fe642f8eb0d04c
Karlan <zngr96@yadnex.ru>
2023-11-03 18:50:51 +0300

    added task2 in lesson7

---
+++ lesson7/task2/Program.cs
@@ -0,0 +1,59 @@
+﻿void PrintArray(int[,] matrix)
+{
+    // Введите свое решение ниже
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < matrix.GetLength(1); m++)
+        {
+            Console.Write($"{matrix[i, m]}\t"); // конвертация строки в вещественное число (string.Format("{0:f}", number))
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateIncreasingMatrix(int n, int m, int k)
+{
+    // Введите свое решение ниже
+    int[,] matrix = new int[n, m];
+    int count = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i, j] = count; // записывае каждое предыдущее число + к
+            count = matrix[i, j] + k; // Кладем в переменную count сумму которая получилась для того, чтобы дальше к ней прибавлять k
+        }
+    }
+    return matrix;
+}
+
+int[] FindNumberByPosition (int [,] matrix, int rowPosition, int columnPosition)
+{
+  // Введите свое решение ниже
+  int[] coord = new int[2];
+  if (rowPosition <= matrix.GetLength(0) && columnPosition <= matrix.GetLength(1)){
+    coord[0] = rowPosition;
+    coord[1] = columnPosition;
+  return coord;
+  }
+  else{
+    coord = new int[1];
+    return coord;
+  }
+}
+int[,] array = CreateIncreasingMatrix(3, 4, 2);
+
+void PrintCheckIfError (int[] results, int X, int Y)
+{
+  // Введите свое решение ниже
+    if (results.Length > 1){
+    int[] arr = new int[2];
+    Console.WriteLine($"The number in [{X}, {Y}] is {array[X,Y]}");
+    }    
+    else Console.WriteLine("There is no such index");
+}
+
+
+PrintArray(array);
+PrintCheckIfError(FindNumberByPosition(array, 8, 3), 8, 3);
