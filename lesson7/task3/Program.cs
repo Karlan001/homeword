31b07b426e0c7572f8ebe1d586f06391e4a70654
Karlan <zngr96@yadnex.ru>
2023-11-19 19:22:00 +0300

    add task3 in lesson8

---
+++ lesson7/task3/Program.cs
@@ -0,0 +1,59 @@
+﻿void PrintArray(int[,] matrix)
+{
+    // Введите свое решение ниже
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int m = 0; m < matrix.GetLength(1); m++)
+        {
+            Console.Write($"{matrix[i, m]}\t"); // конвертация строки в вещественное число (string.Format("{0:f}", number))
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] CreateIncreasingMatrix(int n, int m, int k)
+{
+    // Введите свое решение ниже
+    int[,] matrix = new int[n, m];
+    int count = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i, j] = count; // записывае каждое предыдущее число + к
+            count = matrix[i, j] + k; // Кладем в переменную count сумму которая получилась для того, чтобы дальше к ней прибавлять k
+        }
+    }
+    return matrix;
+}
+
+void PrintListAvr(double[] list)
+{
+    // Введите свое решение ниже
+    Console.WriteLine("The averages in columns are:");
+    for (int i = 0; i < list.Length; i++)
+    {
+        Console.Write($"{list[i]:f2}\t");
+    }
+}
+
+double[] FindAverageInColumns(int[,] matrix)
+{
+    // Введите свое решение ниже
+    double[] avarage = new double[matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(1); i++)
+    {   
+        double sum = 0;
+        for (int j = 0; j < matrix.GetLength(0); j++)
+        {
+            sum += matrix[j,i];
+        }
+        avarage[i] = sum / matrix.GetLength(0);
+    }
+    return avarage;
+}        
+
+int[,] result = CreateIncreasingMatrix(3, 4, 2);
+PrintArray(result);
+PrintListAvr(FindAverageInColumns(result));
---
+++ lesson8/task3/Program.cs
@@ -0,0 +1,58 @@
+﻿
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MultiplyIfPossible(int[,] matrixA, int[,] matrixB)
+{
+    if(matrixA.GetLength(1) != matrixB.GetLength(0))
+    {
+        Console.WriteLine("It is impossible to multiply.");
+    }
+    else
+    {
+        int[,] newMat = MatrixMultiplication(matrixA, matrixB);
+        PrintMatrix(newMat);
+    }
+}
+
+int[,] MatrixMultiplication(int[,] matrixA, int[,] matrixB)
+{
+    int[,] newMatrix = new int[matrixA.GetLength(0),matrixB.GetLength(1)];
+    for(int i = 0; i < matrixA.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrixA.GetLength(1); j++)
+        {
+            for(int k = 0; k < matrixA.GetLength(1); k++)
+            {
+            newMatrix[i,j] += matrixA[i,k] * matrixB[k,j];
+            }
+        }
+    }
+    return newMatrix;
+}
+
+int[,] matrixA = new int[,] {
+                {2, 4},
+                {1, 3},
+                {5, 6}
+            };
+
+int[,] matrixB = new int[,] {
+                {5, 6},
+                {7, 8}
+            };
+PrintMatrix(matrixA);
+Console.WriteLine();
+PrintMatrix(matrixB);
+Console.WriteLine();
+MultiplyIfPossible(matrixA, matrixB);
